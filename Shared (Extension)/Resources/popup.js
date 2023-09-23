cca759c2f5806864d9110b3676cb1aa37e6dc890
J <>
2023-09-23 21:38:59 +0800

    batman

---
+++ Shared (Extension)/Resources/popup.js
@@ -0,0 +1,11 @@
+console.log("Hello World!", browser);
+
+const configuration = {
+	'hide-shorts': false,
+	'low-case-title': false
+}
+
+Object.keys(configuration).forEach
+
+extension.settings.setItem(key, value)
+extension.settings.getItem(key)
